34f12b86399c85ab0498397a16340861478db61d
SLJDivyanjalee <as20240582.sci.sjp.ac.lk>
2025-07-08 09:05:41 +0530

    initial commit

---
+++ Project1_csc1031.0/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{int i;
+    for(i=0; i<=3; i++){
+    printf("Hello world!\n");
+    printf("\tHello world!\n");
+    printf("\t\tHello world!\n");
+    printf("\tHello world!\n");
+    printf("Hello world!\n");
+}
+    return 0;
+}
